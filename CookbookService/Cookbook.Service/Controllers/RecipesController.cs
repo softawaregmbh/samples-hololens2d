c14b4dbf2939d5e8643ffe3a16726f50ba47654d
Dominik Dörr <dominik.doerr@softaware.at>
2019-09-17 10:15:25 +0200

    removed not needed packages, update to 4.6.2, update packages

--- CookbookService/Cookbook.Service/Controllers/RecipesController.cs
+++ CookbookService/Cookbook.Service/Controllers/RecipesController.cs
@@ -1,11 +1,10 @@
-﻿using Cookbook.Service.Data.Domain;
-using Cookbook.Service.Data.Repository;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using Cookbook.Service.Data.Domain;
+using Cookbook.Service.Data.Repository;
 
 namespace Cookbook.Service.Controllers
 {
--- CookbookService/Cookbook.Service/Controllers/SearchController.cs
+++ CookbookService/Cookbook.Service/Controllers/SearchController.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 using Cookbook.Service.Data.Domain;
 using Cookbook.Service.Data.Repository;
 
